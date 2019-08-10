09cfd5650596bdcf04ba1ae9c3be6341b1bfd650
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-10 18:01:46 +0900

    Mutated by booking trips

--- src/schema.js
+++ src/schema.js
@@ -50,7 +50,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
